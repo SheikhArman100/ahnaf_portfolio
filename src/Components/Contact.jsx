26d14e5ba585709b6ffa539e52d8cc5ed51c8a14
Sheikh Arman <sheikharman100@gmail.com>
2023-03-01 13:03:19 +0600

    overflow asjusted

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -15,23 +15,23 @@ function Contact() {
       <h3 className="text-bases mt-2 text-center font-medium ">
         come and visit my office
       </h3>
-      <div className="flex  items-center justify-center gap-x-20 sm:gap-x-1 md:gap-x-3 ">
-        <div className="flex aspect-square w-40 flex-[1_1_auto] flex-col items-center rounded-lg  py-6 px-2 text-center text-bgColor sm:px-1 sm:py-3">
+      <div className="flex items-center justify-center gap-x-10 sm:gap-x-1 md:flex-col md:gap-x-3 ">
+        <div className="flex  h-40 w-40 flex-[1_1_auto] flex-col items-center rounded-lg  py-6 px-2 text-center text-bgColor sm:px-1 sm:py-3 md:h-28 md:w-full md:py-2">
           <IoLocationSharp size={40} />
-          <span className="font-bold md:text-sm">Location</span>
+          <span className="font-bold sm:text-xs md:text-sm">Location</span>
           <p className="mt-2 font-secondText text-xs ">
             House #22, Road #23, Block #B, Banani, Dhaka-1213
           </p>
         </div>
-        <div className="flex aspect-square w-40 flex-[1_1_auto] flex-col items-center rounded-lg  py-6 px-2 text-center text-bgColor">
+        <div className="flex aspect-square h-40 w-40 flex-[1_1_auto] flex-col items-center rounded-lg py-6  px-2 text-center text-bgColor md:h-28 md:py-2">
           <CgPhone size={40} />
-          <span className="font-bold md:text-sm">Phone</span>
+          <span className="font-bold sm:text-xs md:text-sm">Phone</span>
           <p className=" mt-2 font-secondText text-xs ">0189 482 9666</p>
           <p className="font-secondText text-xs ">0176 865 4422</p>
         </div>
-        <div className="flex aspect-square w-40 flex-[1_1_auto] flex-col items-center rounded-lg  py-6 px-2 text-center text-bgColor">
+        <div className="flex aspect-square h-40 w-40 flex-[1_1_auto] flex-col items-center  rounded-lg py-6 px-2 text-center text-bgColor md:h-28 md:py-2">
           <MdEmail size={40} />
-          <span className="font-bold md:text-sm">Email Address</span>
+          <span className="font-bold sm:text-xs md:text-sm">Email Address</span>
           <p className="mt-2 font-secondText text-xs">infomusabd@gmail.com</p>
           <p className="font-secondText text-xs">info@musabd.net</p>
         </div>
